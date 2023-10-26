35c8beaa4619ab39e21a3e1e38304fcd0e734449
deepak15341 <deepakkumarsahbgp123@gmail.com>
2023-10-26 18:32:35 +0530

    initial

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,11 +1,15 @@
+import 'package:bitcoin_ticker/coin_data.dart';
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
+  const PriceScreen({super.key});
+
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedValue = 'USD';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +46,28 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: const EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              dropdownColor: Colors.grey,
+              borderRadius: BorderRadius.circular(10),
+              iconDisabledColor: Colors.white,
+              iconEnabledColor: Colors.white54,
+              iconSize: 50,
+              focusColor: Colors.white,
+              value: selectedValue,
+
+                items: currenciesList.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+
+              onChanged: (String? value) {
+                setState(() {
+                  selectedValue =value!;
+                });
+              },
+            ),
           ),
         ],
       ),
